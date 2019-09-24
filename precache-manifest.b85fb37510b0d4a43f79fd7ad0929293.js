5dab2bb9c297301dba048c46623c7ceefc132b2c
JuCJeff <zma67@wisc.edu>
2019-09-23 20:33:10 -0500

    Updates

---
+++ precache-manifest.b85fb37510b0d4a43f79fd7ad0929293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9447eb47b9021a1dc594a0d5ac057721",
+    "url": "/nba-shot-data-visualization/index.html"
+  },
+  {
+    "revision": "762ce147432dfb312207",
+    "url": "/nba-shot-data-visualization/static/css/main.765bc80b.chunk.css"
+  },
+  {
+    "revision": "f356c0d87d127d3c1838",
+    "url": "/nba-shot-data-visualization/static/js/2.9eac9aa1.chunk.js"
+  },
+  {
+    "revision": "762ce147432dfb312207",
+    "url": "/nba-shot-data-visualization/static/js/main.d2002695.chunk.js"
+  },
+  {
+    "revision": "7445b2a4364ff54db485",
+    "url": "/nba-shot-data-visualization/static/js/runtime~main.5b038480.js"
+  },
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/nba-shot-data-visualization/static/media/nba-logoman-word-white.9470e2d5.svg"
+  }
+]);
