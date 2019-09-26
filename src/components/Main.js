dcf04107cc38917865e5ef03a2f53629b930c949
JuCJeff <zma67@wisc.edu>
2019-09-25 22:56:12 -0500

    Added search functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,45 @@
-import React from 'react';
-import nba from 'nba';
+import React from "react";
+import nba from "nba";
 
-import Profile from './Profile';
-import DataViewContainer from './DataViewCountainer';
+import Profile from "./Profile";
+import DataViewContainer from "./DataViewCountainer";
+import SearchBar from "./SearchBar";
+import { DEFAULT_PLAYER_INFO } from '../constants';
 
 export class Main extends React.Component {
   state = {
-    playerInfo: {
-      playerId: nba.findPlayer('Paul George').playerId,
-      teamAbbreviation: 'LAC'
-    }
+    playerInfo: DEFAULT_PLAYER_INFO
   };
 
   componentDidMount() {
+    this.loadPlayerInfo("Kawhi Leonard");
+  }
+
+  handleSelectPlayer = playerName => {
+    this.loadPlayerInfo(playerName);
+  }
+
+  loadPlayerInfo = playerName => {
     nba.stats
-      .playerInfo({ PlayerID: nba.findPlayer('Paul George').playerId })
+      .playerInfo({ PlayerID: nba.findPlayer(playerName).playerId })
       .then(info => {
-        console.log(info);
         const playerInfo = Object.assign(
           info.commonPlayerInfo[0],
           info.playerHeadlineStats[0]
         );
-        console.log(playerInfo);
+        console.log(info);
         this.setState({ playerInfo });
       });
-  }
+  };
 
   render() {
     return (
-      <div className='main'>
-        <Profile playerInfo={this.state.playerInfo} />
-        <DataViewContainer playerId={this.state.playerInfo.playerId} />
+      <div className="main">
+        <SearchBar handleSelectPlayer={this.handleSelectPlayer} />
+        <div className="player">
+          <Profile playerInfo={this.state.playerInfo} />
+          <DataViewContainer playerId={this.state.playerInfo.playerId} />
+        </div>
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import nba from "nba";
+
+import { AutoComplete, Input, Icon } from 'antd';
+import { PROFILE_PIC_URL_PREFIX } from '../constants';
+
+const Option = AutoComplete.Option;
+
+export class SearchBar extends React.Component {
+  state = {
+    dataSource: []
+  };
+
+  handleSearch = value => {
+    this.setState({
+      dataSource: !value
+        ? []
+        : nba.searchPlayers(value).map(player => ({
+            fullName: player.fullName,
+            playerId: player.playerId
+          }))
+    });
+  };
+
+  onSelect = (playerName) => {
+      this.props.handleSelectPlayer(playerName);
+  }
+
+  render() {
+    const { dataSource } = this.state;
+    const options = dataSource.map(player => (
+      <Option
+        key={player.fullName}
+        value={player.fullName}
+        className="player-option"
+      >
+        <img
+          className="player-option-image"
+          src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}
+          alt='player profile'
+        />
+        <span className="player-option-label">{player.fullName}</span>
+      </Option>
+    ));
+
+    return (
+      <AutoComplete
+        className="search-bar"
+        size="large"
+        dataSource={options}
+        onSelect={this.onSelect}
+        onSearch={this.handleSearch}
+        placeholder="Search NBA Player"
+        optionLabelProp="value"
+      >
+        <Input
+          suffix={<Icon type="search" className="certain-category-icon" />}
+        />
+      </AutoComplete>
+    );
+  }
+}
+
+export default SearchBar;
